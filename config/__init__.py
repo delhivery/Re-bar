24d6c02ce91e04d94cef2c6ec07af303bdfd56ba
Amit Prakash Ambasta <ambasta@users.noreply.github.com>
2015-10-13 01:05:32 +0530

    Disque hosts to list

--- config/__init__.py
+++ config/__init__.py
@@ -14,14 +14,14 @@ MONGO_URI = 'mongodb://{}/?{}'.format(
 )
 
 DISQUE_HOSTS = [
-    'disque-fap-01.internal.sdb:7711'
-    'disque-fap-02.internal.sdb:7711'
-    'disque-fap-03.internal.sdb:7711'
-    'disque-fap-04.internal.sdb:7711'
-    'disque-fap-05.internal.sdb:7711'
-    'disque-fap-06.internal.sdb:7711'
-    'disque-fap-07.internal.sdb:7711'
-    'disque-fap-08.internal.sdb:7711'
+    'disque-fap-01.internal.sdb:7711',
+    'disque-fap-02.internal.sdb:7711',
+    'disque-fap-03.internal.sdb:7711',
+    'disque-fap-04.internal.sdb:7711',
+    'disque-fap-05.internal.sdb:7711',
+    'disque-fap-06.internal.sdb:7711',
+    'disque-fap-07.internal.sdb:7711',
+    'disque-fap-08.internal.sdb:7711',
 ]
 
 FAP_QUEUE = 'fap_fap_fap'
