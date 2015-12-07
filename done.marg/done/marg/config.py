832c33c17c3aa1254b8900ec96080c19e4cf466d
Amit Prakash Ambasta <ambasta@users.noreply.github.com>
2015-12-07 12:55:03 +0530

    v2 prep

--- done.marg/done/marg/config.py
+++
@@ -1,7 +0,0 @@
-'''
-This module stores the various constant configurations
-used by done.marg
-'''
-DAY_IN_SECONDS = 24 * 3600
-
-TIME_FORMAT = '%H:%M:%S'
--- rebar/api/boto_consumer.py
+++
@@ -1,7 +0,0 @@
-from boto import kinesis
-from rebar.config import AWS_KINESIS_STREAM
-
-kcl = kinesis.layer1.KinesisConnection()
-stream = kcl.describe_stream(AWS_KINESIS_STREAM)
-shards = stream['StreamDescription']['Shards']
-
