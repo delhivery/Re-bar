1b2208f13bbead2b1e95cdc47fc1d277f32cdb0a
Amit Prakash Ambasta <ambasta@users.noreply.github.com>
2016-01-09 14:14:08 +0530

    Fresh start

--- include/utils.hpp
+++
@@ -1,9 +0,0 @@
-#include <boost/date_time.hpp>
-#include <limits>
-#include <constants.hpp>
-
-long abs_durinal(long time);
-
-long wait_time(long t_init, long t_depart);
-
-long time_from_string(std::string datestring);
