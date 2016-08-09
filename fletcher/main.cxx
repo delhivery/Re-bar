5b291f963ce30111803c920b60d6b8180ee58ec4
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2016-08-09 11:38:34 +0530

    Initial server attempt

---
+++ fletcher/main.cxx
@@ -0,0 +1,6 @@
+#include "server/expath-server.hxx"
+
+int main() {
+    RunServer();
+    return 0;
+}
---
+++ fletcher/structures/constants.hxx
@@ -0,0 +1,10 @@
+//
+// Created by amitprakash on 8/8/16.
+//
+
+#ifndef FLETCHER_CONSTANTS_HXX
+#define FLETCHER_CONSTANTS_HXX
+
+const long TIME_DURINAL = 24 * 3600;
+
+#endif //FLETCHER_CONSTANTS_HXX
